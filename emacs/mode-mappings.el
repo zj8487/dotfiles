715ca55c1ae3a8ded69c42175864a94f9c9d93f8
Lars-Magnus Skog <lars.magnus.skog@gmail.com>
2013-02-01 23:40:43 +0100

    add js2-mode as submodule

--- emacs/mode-mappings.el
+++ emacs/mode-mappings.el
@@ -1,6 +1,4 @@
 ;; JavaScript
-;; TODO js2-mode should be a git submodule in site-lisp folder
-(add-to-list 'load-path "~/src/js2-mode")
 (autoload 'js2-mode "js2-mode" nil t)
 (add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
 (add-to-list 'auto-mode-alist '("\\.json$" . javascript-mode))
