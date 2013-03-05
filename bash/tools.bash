d9ab26f084242e8c8682e75b0ace0ece6e083ab2
Lars-Magnus Skog <lars.magnus.skog@gmail.com>
2013-03-05 17:19:38 +0100

    add publish_skynet_object

--- bash/tools.bash
+++ bash/tools.bash
@@ -1,7 +1,16 @@
 #!/bin/bash
 
+function shasum {
+    cat $1 | sha256sum | cut -d' ' -f1
+}
+
 function publish_skynet_protocol {
-    TARGET=`cat $1 | sha256sum | cut -d' ' -f1`
+    TARGET=`shasum $1`
     cp $1 ~/.skynet/protocols/$TARGET
     cp $1 ~/src/p2proto/protocols/$TARGET
+}
+
+function publish_skynet_object {
+    TARGET=`shasum $1`
+    cp $1 ~/.skynet/objects/public/$TARGET
 }
