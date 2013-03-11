dfe9d59edc1509c41acba7bf2c4e94f3612ed080
Lars-Magnus Skog <lars.magnus.skog@gmail.com>
2013-03-11 19:39:00 +0100

    add update_leveldb bash function

--- bash/tools.bash
+++ bash/tools.bash
@@ -14,3 +14,8 @@ function publish_skynet_object {
     TARGET=`shasum $1`
     cp $1 ~/.skynet/objects/public/$TARGET
 }
+
+function update_leveldb {
+    cd ~/src/node-leveldown && git pull && node-gyp rebuild
+    cd ~/src/node-levelup && git pull
+}
