d335fdbdd0090e2faf69f6a010b55ef06d118ad8
Lars-Magnus Skog <lars.magnus.skog@gmail.com>
2013-04-28 04:27:30 +0200

    wrap with tick() and call it every second

--- node/goxticker.js
+++ node/goxticker.js
@@ -8,39 +8,45 @@ var options = {
       , method: 'GET'
     }
 
-var req = http.request(options, function (res) {
-      var result = ''
+function tick() {
+  var req = http.request(options, function (res) {
+        var result = ''
 
-      res.on('data', function (data) {
-        result += data
-      })
+        res.on('data', function (data) {
+          result += data
+        })
 
-      res.on('end', function () {
-        var data = JSON.parse(result)
-        if (data.result && data.result == 'success') {
-          data = data.data
-          var maxLength = maxElementLength(Object.keys(data))
-          Object.keys(data).forEach(function (key) {
-            var value = data[key]
-            if ('object' === typeof value && value.value) {
-              console.log(key + separator(key, maxLength) + value.value)
-            }
-          })
+        res.on('end', function () {
+          var data = JSON.parse(result)
+          if (data.result && data.result == 'success') {
+            data = data.data
+            var maxLength = maxElementLength(Object.keys(data))
+            Object.keys(data).forEach(function (key) {
+              var value = data[key]
+              if ('object' === typeof value && value.value) {
+                console.log(key + separator(key, maxLength) + value.value)
+              }
+            })
+          }
+        })
+
+        function maxElementLength(array) {
+          array = array.map(function (el) { return el.length })
+          return Math.max.apply(null, array)
+        }
+
+        function separator(key, max) {
+          var result = ': '
+          for (var i = 0; i < max - key.length; ++i) {
+            result += ' '
+          }
+          return result
         }
       })
 
-      function maxElementLength(array) {
-        array = array.map(function (el) { return el.length })
-        return Math.max.apply(null, array)
-      }
+  req.end()
 
-      function separator(key, max) {
-        var result = ': '
-        for (var i = 0; i < max - key.length; ++i) {
-          result += ' '
-        }
-        return result
-      }
-    })
+  setTimeout(tick, 1000)
+}
 
-req.end()
+tick()
