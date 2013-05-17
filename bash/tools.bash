287a85b310a799226f46665e446c745ec154b510
Lars-Magnus Skog <lars.magnus.skog@gmail.com>
2013-05-18 00:32:13 +0200

    uncomment old skynet stuff, update-leveldb should pushd and popd

--- bash/tools.bash
+++ bash/tools.bash
@@ -1,23 +1,25 @@
 #!/bin/bash
 
-function shasum {
-    cat $1 | sha256sum | cut -d' ' -f1
-}
+#function shasum {
+#    cat $1 | sha256sum | cut -d' ' -f1
+#}
 
-function publish_skynet_protocol {
-    TARGET=`shasum $1`
-    cp $1 ~/.skynet/protocols/$TARGET
-    cp $1 ~/src/p2proto/protocols/$TARGET
-}
+#function publish_skynet_protocol {
+#    TARGET=`shasum $1`
+#    cp $1 ~/.skynet/protocols/$TARGET
+#    cp $1 ~/src/p2proto/protocols/$TARGET
+#}
 
-function publish_skynet_object {
-    TARGET=`shasum $1`
-    cp $1 ~/.skynet/objects/public/$TARGET
-}
+#function publish_skynet_object {
+#    TARGET=`shasum $1`
+#    cp $1 ~/.skynet/objects/public/$TARGET
+#}
 
-function update_leveldb {
+function update-leveldb {
+    pushd .
     cd ~/src/node-leveldown && git pull && node-gyp rebuild
     cd ~/src/node-levelup && git pull
+    popd
 }
 
 function run-bitcoin {
