0a09a32e837338fb9bdd9285209b2290cdf70264
ralphtheninja <ralphtheninja@riseup.net>
2014-08-26 00:04:34 +0200

    json and gyp uses js2-mode, disable nodejs-repl

--- emacs/mode-mappings.el
+++ emacs/mode-mappings.el
@@ -1,7 +1,8 @@
 ;; JavaScript
 (autoload 'js2-mode "js2-mode" nil t)
 (add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
-(add-to-list 'auto-mode-alist '("\\.json$" . javascript-mode))
+(add-to-list 'auto-mode-alist '("\\.json$" . js2-mode))
+(add-to-list 'auto-mode-alist '("\\.gyp$" . js2-mode))
 (add-to-list 'magic-mode-alist '("#!/usr/bin/env node" . js2-mode)) ; What is this?
 
 (provide 'mode-mappings)
