95991c95f8ab3a4c3438314ff69e9c5bc3784178
Lars-Magnus Skog <ralphtheninja@riseup.net>
2015-01-20 17:56:50 +0100

    add script for cloning/updating repositories related to leveldb

---
+++ bash/clone.sh
@@ -0,0 +1,11 @@
+function clone_remote {
+    local repo=$1
+    local org=$2
+    if [ ! -d $repo ]; then
+        git clone git@github.com:ralphtheninja/$repo && cd $repo
+        git remote add upstream git@github.com:$org/$repo && git fetch --all && cd ..
+    else
+        echo "Updating $repo .."
+        cd $repo && git fetch --all && cd ..
+    fi
+}
